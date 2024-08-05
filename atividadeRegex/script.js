dd5943cee97f8a840860921f7e7b866011bcfec3
Guilherme Cesar Tomiasi <gtomiasi@gmail.com>
2024-08-05 18:40:42 -0300

    regExp verifier

---
+++ atividadeRegex/script.js
@@ -0,0 +1,52 @@
+/**
+ * @type HTMLInputElement
+ */
+const inputRegex = document.getElementById("input_regex");
+/**
+ * @type HTMLInputElement
+ */
+const inputTest = document.getElementById("input_test");
+
+// Resetting
+inputRegex.value = "^Exemplo$"
+inputTest.value = "Exemplo"
+
+let regexIsValid = true;
+let regexObject = new RegExp("");
+
+function validateRegExpInput() {
+  if (inputRegex.value == "") {
+    inputRegex.style.color = "black";
+    regexIsValid = true;
+    return;
+  }
+
+  try {
+    regexObject = new RegExp(inputRegex.value);
+    inputRegex.style.color = "green";
+    regexIsValid = true;
+  } catch(e) {
+    inputRegex.style.color = "red";
+    regexIsValid = false;
+  }
+}
+
+function validateTestInput() {
+  if (regexIsValid) {
+    if (regexObject.test(inputTest.value)) {
+      inputTest.style.color = "green";
+    } else {
+      inputTest.style.color = "red";
+    }
+  }
+}
+
+function validateBoth() {
+  validateRegExpInput();
+  validateTestInput();
+}
+
+inputRegex.addEventListener("input", validateBoth);
+inputTest.addEventListener("input", validateTestInput);
+
+validateBoth();
